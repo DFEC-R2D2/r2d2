457ffdc32a408de980f2a90efc901f7e07099a85
dfecr2d2 <dfecr2d2@users.noreply.github.com>
2017-08-10 20:23:59 -0600

    update

--- tests/camera_test.py
+++ tests/camera_test.py
@@ -8,9 +8,10 @@ from __future__ import print_function
 import cv2
 
 cam = cv2.VideoCapture(0)  # grab from default camera
-s, im = cam.read()         # captures image
+ret, im = cam.read()         # captures image
 # cv2.imshow("Test", im)     # displays captured image
-if s:
+if ret:
+	print('writting image to test.jpg')
 	cv2.imwrite("test.jpg", im)  # writes image test.bmp to disk
 else:
 	print('Failed to get image')
